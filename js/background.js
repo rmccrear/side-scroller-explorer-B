01a189356f814f2c89267d6410faf6598f75ea73
ClayAucoin <clay0aucoin@gmail.com>
2025-08-04 21:29:53 -0500

    added sun to background

--- js/background.js
+++ js/background.js
@@ -4,8 +4,9 @@ function drawBackground() {
     background(135, 206, 235);
     
     // TODO: Sun on the right side
+    fill(255, 255, 0);
     ellipse(400, 0, 50, 50);
-    
+
     // Grass at the bottom (100px high)
     fill(34, 139, 34);
     rect(0, 400 - 100, 400, 100);
