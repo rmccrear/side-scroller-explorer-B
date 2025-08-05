b2fc8e4d15bec3bbf6738134a8af915d10ae95a3
Edward Smith <213342565+EdSmith2032@users.noreply.github.com>
2025-08-04 21:06:12 -0500

    stopped player velocity +320

--- js/user-controls.js
+++ js/user-controls.js
@@ -3,11 +3,14 @@ function respondToUser() {
   
   // TODO: if player is back to position
   // stop them
-  
+   if (player.y>320){
+    player.velocityY = 0;   
+   }
   // Player jumping controls
   if (keyWentDown("UP")) {
     player.velocityY = -4;
   }
+  
   
   // Set velocityY to 4 when player reaches y=100
   if (player.y <= 100) {
