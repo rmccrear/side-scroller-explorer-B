5fcfc3b08e49de3c09bb2e19510fb87c94633b72
ClayAucoin <clay0aucoin@gmail.com>
2025-08-05 21:18:01 -0500

    chagned "400" ro variable on sprite-setup.js

--- js/sprite-setup.js
+++ js/sprite-setup.js
@@ -5,17 +5,17 @@ function setupSprites() {
   health = 100; // Initialize health
   
   // Create food sprite - moving from right to left
-  food = createSprite(400, 150, 30, 30);
+  food = createSprite(screenRight, 150, 30, 30);
   food.addAnimation('food', foodAnimation);
   food.velocityX = -1;
   food.velocityY = 0;
   
   // Create player sprite
-  player = createSprite(200, 320, 40, 40);
+  player = createSprite(screenRight/2, 320, 40, 40);
   player.addAnimation('player', playerAnimation);
     
   // Create enemy sprite - moving from right to left
-  enemy = createSprite(400, 320, 35, 35);
+  enemy = createSprite(screenRight, 320, 35, 35);
   enemy.addAnimation('enemy', enemyAnimation);
   enemy.velocityX = -5;
   enemy.velocityY = 0;
