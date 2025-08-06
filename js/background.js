0fddbfada5abba9a3304acf32c2fa412fd188de1
ClayAucoin <clay0aucoin@gmail.com>
2025-08-05 20:02:49 -0500

    removed game over from background.js

--- js/background.js
+++ js/background.js
@@ -10,20 +10,4 @@ function drawBackground() {
     // Grass at the bottom (100px high)
     fill(34, 139, 34);
     rect(0, 400 - 100, 400, 100);
-
-    // GAME OVER
-    if (health <=0) {
-        background("black");
-        textSize(50);
-        fill("#008000");
-        text("Game Over!", 50, 200);
-        player.visible = false;
-        food.visible = false;
-        enemy.visible = false;
-    } else {
-        player.visible = true;
-        food.visible = true;
-        enemy.visible = true;
-    }
-
 }
